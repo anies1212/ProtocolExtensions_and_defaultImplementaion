2f2ea2b0a33287d4c95c18e49462e4cdcb98f4e2
arakaki_seina <87635517+anies1212@users.noreply.github.com>
2022-08-30 20:44:24 +0900

    Initial Commit

---
+++ ProtocolExtensions_and_DefaultsImplementation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolExtensions_and_DefaultsImplementation
+//
+//  Created by 新垣 清奈 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
